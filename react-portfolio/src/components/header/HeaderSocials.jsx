537d44d31a199f36109523914b66c99005ade996
Kean Bayneslow <kean.bayneslow@gmail.com>
2022-12-28 09:47:15 +1100

    Completed Headrer Section

--- react-portfolio/src/components/header/HeaderSocials.jsx
+++ react-portfolio/src/components/header/HeaderSocials.jsx
@@ -5,7 +5,7 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href="www.linkedin.com/in/kean-bayneslow" target="_blank"><BsLinkedin/></a>
+        <a href="https://www.linkedin.com/in/kean-bayneslow/" target="_blank"><BsLinkedin/></a>
         <a href="https://github.com/keanbayneslow" target="_blank"><BsGithub/></a>
         
     </div>
--- react-portfolio/src/components/header/header.jsx
+++ react-portfolio/src/components/header/header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import Kean from '../../assets/Kean.JPG'
 import HeaderSocials from './HeaderSocials'
+import Keanbg from '../../assets/Keanbg.png'
 
 
 
@@ -12,12 +12,12 @@ const header = () => {
       <div className="container header_container">
 <h5>Hello I'm </h5>
 <h1>Kean Bayneslow</h1>
-<h5 className='text-light'>Shift Supervisor</h5>
+<h5 className='text-light'>Shift Supervisor - Supporting Safety, Health and Wellbeing with Sonder</h5>
 <CTA />
 <HeaderSocials />
 
-<div className="Kean">
-  <img src={Kean} alt="Kean Bayneslow" />
+<div className="Keanbg">
+  <img src={Keanbg} alt="Kean Bayneslow" />
 </div>
 
 <a href="#contact" className='scroll_down'>Scroll Down</a>
