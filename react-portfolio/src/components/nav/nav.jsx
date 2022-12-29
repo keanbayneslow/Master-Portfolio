3c726400830d544862026cf3042583e9b187d08a
Kean Bayneslow <kean.bayneslow@gmail.com>
2022-12-29 11:43:09 +1100

    completed nav bar react hooks

--- react-portfolio/src/components/nav/nav.jsx
+++ react-portfolio/src/components/nav/nav.jsx
@@ -7,17 +7,17 @@ import {GiBrainTentacle} from 'react-icons/gi'
 import {AiOutlineContacts} from 'react-icons/ai'
 import {useState} from 'react'
 
-const nav = () => {
+const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><AiOutlineBook/></a>
-      <a href="#skills"><GiBrainTentacle/></a>
-      <a href="#contact"><AiOutlineContacts/></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineBook/></a>
+      <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><GiBrainTentacle/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineContacts/></a>
     </nav>
   )
 }
 
-export default nav
+export default Nav
