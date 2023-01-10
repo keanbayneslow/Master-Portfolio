317ebf8ee0c492b0c8452a18dc42daff7219c9bb
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-01-10 20:49:06 +1100

    added contact and footer section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlinePhone} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-const contact = () => {
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_h2n35zg', 'template_h16qs1g', form.current, 'vxOtjq6xoDPozBlfO')
+
+    e.target.reset()
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>contact
+    <h5>Get In Touch</h5>
+    <h2>Contact Me</h2>
+    
+      <div className="container contact_container"> 
+        <div className="contact_options"> 
+           <article className="contact_option">
+            <MdOutlineEmail className='contact_option-icon'/>
+              <h4>Email</h4>
+              <h5>kean.bayneslow@gmail.com</h5>
+              <a href="mailto:kean.bayneslow@gmail.com" target="_blank">Send a Message</a>
+            </article>
+            <article className="contact_option">
+            <AiOutlinePhone className='contact_option-icon'/>
+              <h4>Phone</h4>
+              <h5>0415 210 285</h5>
+              
+            </article>
+        </div>
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name='name' placeholder='Your Full Name' required />
+            <input type="email" name='email' placeholder='Your Email' required />
+            <textarea name='message' rows="7" placeholder='Your Message' required></textarea>
+            <button type='submit' className='btn btn-primary'>Send Message</button>
+          </form> 
+      </div>
+    </section>
   )
 }
 
-export default contact
+export default Contact
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
+import Logo from '../../assets/Logo.png'
+import Logo2 from '../../assets/Logo2.png'
 
-const footer = () => {
+const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <img className='footer_logo-img' src={Logo} alt="Kean Logo"/>
+      <img className='footer_logo-imgb' src={Logo2} alt="Kean Logo"/>
+      <a href="" className='footer_logo'>Kean Bayneslow</a>
+      
+
+      <ul className='permalinks'>
+        <li> <a href="#">Home</a> </li>
+        <li> <a href="#about">About</a> </li>
+        <li> <a href="#experience">Experience</a> </li>
+        <li> <a href="#skills">Skills</a> </li>
+        <li> <a href="#portfolio">Portfolio</a> </li>
+        <li> <a href="#quotes">Quotes</a> </li>
+        <li> <a href="#contact">Contact</a> </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/kean-bayneslow/"><BsLinkedin/></a>
+        <a href="https://github.com/keanbayneslow"><BsGithub/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; Kean Bayneslow - All rights reserved</small>
+      </div>
+
+
+
+    </footer>
   )
 }
 
-export default footer
+export default Footer
